77b13f00d0fb3bd5fb43c63f0e5082dd62ccb584
Alexandr <XoxlofAlexandr@yandex.ru>
2019-02-21 01:06:33 +0300

    Добавлена сортировка по убыванию и по уменьшению. Решен баг с нерабочими кнопками после сортировки

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,28 @@
+$(document).ready(function(){
+
+	$(".asc").click(function() {
+		var divList = $(".card");
+        divList.sort(function(a, b){
+            var result = parseFloat($(a).find('[data-price]').data('price')) - parseFloat($(b).find('[data-price]').data('price'));
+            return result;
+        });
+        $(".cards__list").html(divList);
+    });
+    
+    $(".desc").click(function() {
+		var divList = $(".card");
+        divList.sort(function(a, b){
+            var result = parseFloat($(b).find('[data-price]').data('price')) - parseFloat($(a).find('[data-price]').data('price'));
+            return result;
+        });
+        $(".cards__list").html(divList);
+    });
+
+});
+
 $(document).ready(function() {
     var count = 0;
-    $('.button').click(function() {
+    $(document).on('click', '.button', function () {
         $(this).toggleClass('active');
         if ( $(this).hasClass('active') ) {
             $(this).text('В корзине');
